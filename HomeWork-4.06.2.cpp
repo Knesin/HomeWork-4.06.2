b2d73c889cf5efbab40e019d31e09418684aeb75
Maksim Kl <GuruHacker@yandex.ru>
2023-05-31 12:37:15 +0300

    Initial commit

---
+++ HomeWork-4.06.2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <set>
+
+int main()
+{
+    int count;
+    std::cout << "[IN]: " << std::endl;
+    std::cin >> count;
+    std::set<int> si;
+    for (int i = 0; i < count; ++i) {
+        int in_i;
+        std::cin >> in_i;
+        si.insert(in_i);
+    }
+    std::cout << "[OUT]: " << std::endl;
+    for (auto i = si.rbegin(), si_rend = si.rend(); i != si_rend; ++i) {
+        std::cout << *i <<std::endl;
+    }
+}
